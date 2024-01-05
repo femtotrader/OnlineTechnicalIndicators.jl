694495dc2546de5b1858bac6bf7aa1a216c9bd15
femtotrader <femto.trader@gmail.com>
2024-01-05 18:44:03 +0100

    WIP

--- src/other/iterators.jl
+++ src/other/iterators.jl
@@ -1,3 +1,4 @@
+using OnlineStatsBase
 using IncTA
 using IncTA: TechnicalIndicator
 using IncTA.SampleData: OPEN_TMPL, HIGH_TMPL, LOW_TMPL, CLOSE_TMPL, VOLUME_TMPL, DATE_TMPL
@@ -7,71 +8,68 @@ mutable struct TechnicalIndicatorIterator{T, I}
     indicator_type::T
     args::Tuple
     kwargs::Base.Pairs
-    iterable_input::I  # should be iterable
+    iterable_input
+    input_iterator #::I  # should be iterable
     indicator_instance::TechnicalIndicator
 
     function TechnicalIndicatorIterator(indicator_type, iterable_input, args...; kwargs...)
         ind = indicator_type{eltype(iterable_input)}(args...; kwargs...)
-        new{typeof(indicator_type), typeof(iterable_input)}(indicator_type, args, kwargs, iterable_input, ind)
+        input_iterator = Iterators.Stateful(iterable_input)
+        new{typeof(indicator_type), typeof(input_iterator)}(indicator_type, args, kwargs, iterable_input, input_iterator, ind)
     end
 
 end
 
 function Base.iterate(itr::TechnicalIndicatorIterator, state=0)
-    iter_result = iterate(itr.iterable_input, state)
-    return iter_result
-    #if state < 0
-    #    
-    #end
-    #println(itr)
-    #iter_result = iterate(itr.iterable_input)
-    #while iter_result !== nothing
-    #    (element, state) = iter_result
-    #    #println(state)
-    #    #fit!(itr.indicator_instance, element)
-    #    #println(element, "\t", value(itr.indicator_instance))
-    #    #return value(itr.indicator_instance)
-    #    iter_result = iterate(itr.iterable_input, state)
-    #end
-    #=
-    iter_result = iterate(itr.input, state)
+    iter_result = iterate(itr.input_iterator, state)
     if iter_result !== nothing
-        println("not")
-        println(iter_result)
-        #return ()
-    else
-        return nothing
+        (element, state) = iter_result
+        state = nobs(itr.indicator_instance)
+        fit!(itr.indicator_instance, element)
+        return (value(itr.indicator_instance), state)
     end
-    =#
 end
 
-Base.length(itr::TechnicalIndicatorIterator) = length(itr.iterable_input)
+# Base.eltype(::Type{TechnicalIndicatorIterator}) = Float64
+# Base.IteratorEltype(::Type{TechnicalIndicatorIterator}) = Base.HasEltype()
+# Base.IteratorSize(::Type{TechnicalIndicatorIterator}) = Base.IsInfinite()
 
-#Base.IteratorSize(::Type{TechnicalIndicatorIterator})
+function Iterators.reset!(itr::TechnicalIndicatorIterator)
+    Iterators.reset!(itr.input_iterator)
+    itr.indicator_instance = itr.indicator_type{eltype(itr.iterable_input)}(itr.args...; itr.kwargs...)
+end
 
+Base.length(itr::TechnicalIndicatorIterator) = length(itr.input_iterator)
 
 
 # === Usage
 
 
-#itr = TechnicalIndicatorIterator(SMA{Float64}(), CLOSE_TMPL)
+# SISO indicator
 
-#itr = TechnicalIndicatorIterator(SMA, CLOSE_TMPL, args..., kwargs...)
 itr = TechnicalIndicatorIterator(SMA, CLOSE_TMPL; period = 3)
 
 println("First iteration")
-for val in itr
-    println(val)
+for (i, val) in enumerate(itr)
+    println("$(i): $(val)")
 end
 
 println("")
 
 println("Second iteration")
-for val in itr
-    println(val)
+Iterators.reset!(itr)
+for (i, val) in enumerate(itr)
+    println("$(i): $(val)")
 end
 
 println("")
 
 println("Third iteration with collect")
+# itr = TechnicalIndicatorIterator(SMA, CLOSE_TMPL; period = 3)
+# or
+Iterators.reset!(itr)
+println(collect(itr))
+
+# SIMO indicator
+itr = TechnicalIndicatorIterator(BB, CLOSE_TMPL)
 println(collect(itr))
