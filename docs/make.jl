1cb16ea443a46bf933c28334864a4c4eea4a8025
femtotrader <femto.trader@gmail.com>
2024-01-13 11:31:29 +0100

    make

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,18 @@
 push!(LOAD_PATH, "../src/")
 using IncTA
 using Documenter
-makedocs(sitename = "IncTA.jl", modules = [IncTA], pages = ["Home" => "index.md"])
+makedocs(
+    sitename = "IncTA.jl",
+    modules = [IncTA],
+    pages = [
+        "Home" => "index.md",
+        "Package Features" => "features.md",
+        "Install" => "install.md",
+        "Usage" => "usage.md",
+        "Indicators support" => "indicators_support.md",
+        "Learn more about usage" => "usage_more.md",
+        "Internals" => "internals.md",
+        "Implementing your own indicator" => "implementing_your_indic.md",
+    ],
+)
 deploydocs(; repo = "github.com/femtotrader/IncTA.jl")
